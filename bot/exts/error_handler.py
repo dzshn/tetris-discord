7b9cde2f951daa32e6100bc322b0a392dd0973f6
Sofia "dzshn" N. L <zshn@pm.me>
2021-09-15 00:41:05 -0300

    feat: send traceback for owners

--- bot/exts/error_handler.py
+++ bot/exts/error_handler.py
@@ -1,5 +1,9 @@
+import traceback
+
 import discord
 from discord.ext import commands
+
+from bot.utils.formatting import codeblock
 
 
 class ErrorHandler(commands.Cog):
@@ -8,9 +12,22 @@ class ErrorHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
-        await ctx.send(
-            embed=discord.Embed(color=0xfa5050, title='Error!', description=f'```{error!r}```'[:1800])
-        )
+        if isinstance(error, commands.CommandInvokeError) and await self.bot.is_owner(ctx.author):
+            await ctx.send(
+                embed=discord.Embed(
+                    title='Error!',
+                    color=0xfa5050,
+                    description=codeblock(
+                        '\n'.join(traceback.format_exception(type(error), error, error.__traceback__)),
+                        max_size=4096
+                    )
+                )
+            )
+
+        else:
+            await ctx.send(
+                embed=discord.Embed(color=0xfa5050, title='Error!', description=codeblock(repr(error)))
+            )
 
 
 def setup(bot: commands.Bot):
