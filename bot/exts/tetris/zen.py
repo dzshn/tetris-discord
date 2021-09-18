20ded652a2ec4b7fa98bac56cda53ab6a4b74461
Sofia "dzshn" N. L <zshn@pm.me>
2021-09-18 13:59:31 -0300

    feat/fix: fixup save system check game type, autosave ongoing games, move restart cmd to subcommand

--- bot/exts/tetris/zen.py
+++ bot/exts/tetris/zen.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from tinydb import where
 from tinydb.table import Table
 
@@ -7,37 +7,62 @@ from bot.lib import Controls, Game
 from bot.lib.maps import Encoder
 
 
+class ZenGame(Game):
+    pass
+
+
 class Zen(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.db: Table = bot.db.table('zen')
+        self.autosave.start()
 
-    @commands.command()
-    async def restart(self, ctx: commands.Context):
-        games: dict[int, Game] = self.bot.get_cog('Manager').games
+    @tasks.loop(minutes=5)
+    async def autosave(self):
+        for author, view in self.bot.get_cog('Manager').games:
+            if not isinstance(view.game, ZenGame):
+                continue
 
-        if ctx.author.id not in games:
-            await ctx.send("There isn't a game running!")
+            game: ZenGame = view.game
+            save = {
+                'user_id': author,
+                'board': Encoder.encode(game.board, game.current_piece),
+                'score': game.score,
+                'queue': game.queue,
+                'hold': game.hold,
+                'hold_lock': game.hold_lock
+            }
+
+            self.db.upsert(save, where('user_id') == author)
+
+    @autosave.before_loop
+    async def before_autosave(self):
+        await self.bot.wait_until_ready()
+
+    @commands.group()
+    async def zen(self, ctx: commands.Context):
+        """Start a Zen mode game.
+
+        → 0 Gravity
+          · possibly configurable by the end of 2024
+        → All progress saved
+        → Board can be reset at any time
+          · via `zen restart`
+        """
+        if ctx.invoked_subcommand is not None:
             return
 
-        games[ctx.author.id].game.reset()
-        await games[ctx.author.id].update_message()
-
-    @commands.command()
-    async def zen(self, ctx: commands.Context):
-        games: dict[int, discord.View] = self.bot.get_cog('Manager').games
+        games: dict[int, Controls] = self.bot.get_cog('Manager').games
 
         if ctx.author.id in games:
             await ctx.send("There's already a game running!")
             return
 
-        games[ctx.author.id] = None
-
         embed = discord.Embed(color=0xfa50a0, title='Loading...').set_image(
             url='https://media.discordapp.net/attachments/825871731155664907/884158159537704980/dtc.gif'
         )
         msg = await ctx.send(embed=embed)
-        game = Game(self.bot.config)
+        game = ZenGame(self.bot.config)
         if (save := self.db.get(where('user_id') == ctx.author.id)) is not None:
             game.board, game.current_piece = Encoder.decode(save['board'])
             game.score = save['score']
@@ -62,6 +87,18 @@ class Zen(commands.Cog):
         self.db.upsert(save, where('user_id') == ctx.author.id)
 
         del games[ctx.author.id]
+
+    @zen.command()
+    async def restart(self, ctx: commands.Context):
+        """Restarts current zen game, all score is kept"""
+        games: dict[int, Controls] = self.bot.get_cog('Manager').games
+
+        if ctx.author.id not in games or not isinstance(games[ctx.author.id].game, ZenGame):
+            await ctx.send("There isn't a zen game running!")
+            return
+
+        games[ctx.author.id].game.reset()
+        await games[ctx.author.id].update_message()
 
 
 def setup(bot: commands.Bot):
