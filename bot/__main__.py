d4f81ffc51cf24fe7c2adab226f14850801df114
Sofia "dzshn" N. L <zshn@pm.me>
2021-10-25 01:12:46 -0300

    remove non-working stuff

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,38 +1,22 @@
-import json
-import pathlib
 import pkgutil
 import traceback
 
 import discord
 from discord.ext import commands
-from tinydb import TinyDB
-from tinydb.middlewares import CachingMiddleware
-from tinydb.storages import JSONStorage
 
+from bot import config
 from bot import exts
 
 
 class TetrisBot(commands.Bot):
     def __init__(self):
-        config_path = pathlib.Path('config.json')
-        if not config_path.exists():
-            open('config.json', 'w').write(open('config_defaults.json').read())
-
-        self.config = json.load(open('config_defaults.json')) | json.load(
-            open('config.json')
-        )
-        storage = CachingMiddleware(JSONStorage)
-        # TEMP: This isn't ideal; maybe a time-based subclass instead?
-        storage.WRITE_CACHE_SIZE = (
-            16  # (also, the default is insanely big for this; 1000 ops)
-        )
-        self.db = TinyDB('db.json', storage=storage)
+        # TODO: There should probably be a `prefix` function
 
         super().__init__(
             allowed_mentions=discord.AllowedMentions(
                 everyone=False, users=True, roles=False, replied_user=True
             ),
-            command_prefix=commands.when_mentioned_or(self.config['prefix']),
+            command_prefix=commands.when_mentioned_or(config['prefix']),
             case_insensitive=True,
             activity=discord.Game('Loading...'),
             status=discord.Status.dnd,
@@ -54,10 +38,6 @@ class TetrisBot(commands.Bot):
 
             except commands.ExtensionError:
                 traceback.print_exc()
-
-    async def close(self):
-        await super().close()
-        self.db.close()
 
 
 bot = TetrisBot()
