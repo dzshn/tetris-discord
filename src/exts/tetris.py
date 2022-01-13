e49f2532eab24ca29d40230cc609c49df42a1660
Sofia "dzshn" N. L <zshn@pm.me>
2022-01-13 05:10:58 -0300

    initial command impl

--- src/exts/tetris.py
+++ src/exts/tetris.py
@@ -1,23 +1,31 @@
 import hikari
 import lightbulb
+import tetris
 
 from src.config import config
 
 plugin = lightbulb.Plugin("Tetris", "Play tetris!")
 
 
-@plugin.command
-@lightbulb.option("mode", "What mode to play", default="zen", choices=["zen"])
-@lightbulb.command("play", "Start a game of tetris", auto_defer=True)
-@lightbulb.implements(lightbulb.SlashCommand)
-async def tetris_play(ctx: lightbulb.Context):
-    skin = config["skins"][0]["controls"]
+MOVES = {
+    "left": tetris.Move.left(),
+    "right": tetris.Move.right(),
+    "das-left": tetris.Move.left(10),
+    "das-right": tetris.Move.right(10),
+    "soft-drop": tetris.Move.soft_drop(5),
+    "hard-drop": tetris.Move.hard_drop(),
+    "rotate-ccw": tetris.Move.rotate(-1),
+    "rotate-cw": tetris.Move.rotate(+1),
+    "rotate-2cw": tetris.Move.rotate(+2),
+    "swap": tetris.Move.swap(),
+}
 
-    layout = [
-        ["noop", "hard-drop", "noop1", "swap", "rotate-2cw"],
-        ["left", "soft-drop", "right", "rotate-ccw", "rotate-cw"],
-    ]
 
+def build_components(
+    ctx: lightbulb.Context,
+    layout: list[list[str]],
+    emojis: dict[str, str],
+) -> list[hikari.api.ActionRowBuilder]:
     rows = []
 
     for i in layout:
@@ -33,13 +41,38 @@ async def tetris_play(ctx: lightbulb.Context):
             else:
                 (
                     row.add_button(hikari.ButtonStyle.PRIMARY, j)
-                    .set_label(skin[j])
+                    .set_label(emojis[j])
                     .add_to_container()
                 )
 
         rows.append(row)
 
-    resp = await ctx.respond("\u200c", components=rows)
+    return rows
+
+
+@plugin.command
+@lightbulb.option("mode", "What mode to play", default="zen", choices=["zen"])
+@lightbulb.command("play", "Start a game of tetris", auto_defer=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def tetris_play(ctx: lightbulb.Context):
+    skin = config["skins"][0]
+
+    game = tetris.BaseGame()
+    embed = hikari.Embed(description=game.render(tiles=skin["pieces"], lines=14))
+
+    embed.set_footer(
+        text=ctx.author.username,
+        icon=ctx.author.avatar_url or ctx.author.default_avatar_url,
+    )
+
+    layout = [
+        ["noop", "hard-drop", "noop1", "swap", "rotate-2cw"],
+        ["left", "soft-drop", "right", "rotate-ccw", "rotate-cw"],
+    ]
+
+    resp = await ctx.respond(
+        embed=embed, components=build_components(ctx, layout, skin["controls"])
+    )
 
     message = await resp.message()
 
@@ -51,17 +84,17 @@ async def tetris_play(ctx: lightbulb.Context):
         )
     ) as stream:
         async for event in stream:
+            cid = event.interaction.custom_id
+            game.push(MOVES[cid])
+            embed.description = game.render(tiles=skin["pieces"], lines=14)
+
             try:
                 await event.interaction.create_initial_response(
-                    hikari.ResponseType.MESSAGE_UPDATE,
-                    content=event.interaction.custom_id,
+                    hikari.ResponseType.MESSAGE_UPDATE, embed=embed
                 )
 
             except hikari.NotFoundError:
-                await event.interaction.edit_initial_response(
-                    event,
-                    content=event.interaction.custom_id,
-                )
+                await event.interaction.edit_initial_response(embed=embed)
 
 
 def load(bot: lightbulb.BotApp):
